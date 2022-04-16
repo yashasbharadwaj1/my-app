7f3ff251e41f97655dc9c835f4b0aec52e03d8fb
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-04-16 10:33:22 +0530

    fourth

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -31,113 +31,19 @@ import {
 import {Header} from "../components"
 
 
-const TabButton = ({label,icon,isFocused,onPress}) => {
-    return(
-        <TouchableWithoutFeedback
-        onPress={onPress}
-        >
-        <Animated.View  style={{
-           flex:1,
-           alignItems: 'center',
-           justifyContent: 'center'
-        }}
-        
-        >
-        <Animated.View  style={{
-            flexDirection:'row',
-            width:"80%",
-            height:50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius:25
+
+
+
+
+
+const MainLayout = ({navigation}) => {
+   
   
-          }}
-        >
-        <Image
-            source={icon}
-            style={{
-                width:20,
-                height:20,
-                tintColor:COLORS.gray
-            }}
-        />
-            {isFocused && 
-            <Text
-                numberOfLines={1}
-                style={{
-                    marginLeft:SIZES.base,
-                    color:COLORS.gray,
-                    ...FONTS.h3
-                }}
-             >
-                 {label}
-            </Text>
-            }
-            </Animated.View>
-            </Animated.View>
-
-        </TouchableWithoutFeedback>
-    )
-}
-
-
-
-
-
-const MainLayout = ({navigation,selectedTab,setSelectedTab}) => {
-    React.useEffect(() => {
-       setSelectedTab(constants.screens.home)
-    },[])
-    
-    
     return (
-        <Animated.View
+        <View>
         
-        style={{
-            flex:1,
-            backgroundColor:COLORS.white
-        }}
         
-        >
-            {/*FOOTER*/}
-            <View style={{
-                height:100,
-                justifyContent: 'flex-end'
-            }}>
-            
-
-                  {/* tabs*/}
-                  <View
-                  
-                  style={{
-                    flex:1,
-                    flexDirection: 'row',
-                    paddingHorizontal:SIZES.radius,
-                    paddingBottom:10,
-                    borderTopLeftRadius:20,
-                    backgroundColor: COLORS.white
-                  }}
-                 
-                 >
-                     <TabButton 
-                     label={constants.screens.home}
-                     icon={icons.home}
-                     isFocused={selectedTab == constants.screens.home}
-                     onPress={()=>setSelectedTab(constants.screens.home)}
-
-                     />
-                    <TabButton 
-                     label={constants.screens.search}
-                     icon={icons.search}
-                     isFocused={selectedTab == constants.screens.search}
-                     onPress={()=>setSelectedTab(constants.screens.search)}
-
-                     />
-
-
-                  </View>
-            </View>
-        </Animated.View>
+        </View>
             
            
         
